94131238ee42238a0748eb57719215e8be6195dd
Mario Schillermann <mario.schillermann@gmail.com>
2025-10-23 15:18:46 +0200

    Turn off all output processing
    It translates each newline ("\n") we print into a carriage return followed by a newline ("\r\n").
    The terminal requires both of these characters in order to start a new line of text.
    The carriage return moves the cursor back to the beginning of the current line, and the newline moves the cursor down a line, scrolling the screen if necessary.
    These two distinct operations originated in the days of typewriters and teletypes.

--- terminal_signals_disable_unix.go
+++ terminal_signals_disable_unix.go
@@ -1,31 +1,29 @@
 //go:build !windows
 
-/*
-Disables default terminal signal and flow-control keys:
-  Ctrl-C   - normally sends SIGINT (interrupt)
-  Ctrl-Z   - normally sends SIGTSTP (suspend)
-  Ctrl-S   - pauses output (XOFF)
-  Ctrl-Q   - resumes output (XON)
-  Ctrl-V   - quotes next character literally
-  CR-to-NL - translation (Ctrl-M fix) via ICRNL
-
-These behaviors are disabled by clearing the following termios flags:
-  ISIG   – disables signal generation (Ctrl-C, Ctrl-Z, Ctrl-\)
-  IXON   – disables software flow control (Ctrl-S, Ctrl-Q)
-  IEXTEN – disables extended input processing (Ctrl-V “literal next”)
-  ICRNL  – disables carriage return to newline translation (prevents Ctrl-M and Enter from both producing '\n')
-*/
-
 package main
 
 import (
 	"golang.org/x/sys/unix"
 )
 
-// function disables:
-// - Ctrl-C (SIGINT) and Ctrl-Z (SIGTSTP) via ISIG
-// - Ctrl-S / Ctrl-Q software flow control via IXON
-// - Ctrl-V "literal next" quoting behavior via IEXTEN
+/*
+Disables default terminal signal and flow-control keys:
+
+	Ctrl-C   - normally sends SIGINT (interrupt)
+	Ctrl-Z   - normally sends SIGTSTP (suspend)
+	Ctrl-S   - pauses output (XOFF)
+	Ctrl-Q   - resumes output (XON)
+	Ctrl-V   - quotes next character literally
+	CR-to-NL - translation (Ctrl-M fix) via ICRNL
+
+These behaviors are disabled by clearing the following termios flags:
+
+	ISIG   – disables signal generation (Ctrl-C, Ctrl-Z, Ctrl-\)
+	IXON   – disables software flow control (Ctrl-S, Ctrl-Q)
+	IEXTEN – disables extended input processing (Ctrl-V “literal next”)
+	ICRNL  – disables carriage return to newline translation (prevents Ctrl-M and Enter from both producing '\n')
+	OPOST  – disables all output processing (prevents "\n" from becoming "\r\n")
+*/
 func terminalSignalsDisable(fd int) error {
 	// Get current terminal attributes
 	termios, err := unix.IoctlGetTermios(fd, unix.TCGETS)
@@ -44,6 +42,9 @@ func terminalSignalsDisable(fd int) error {
 
 	// Disable carriage return to newline translation (fix Ctrl-M)
 	termios.Iflag &^= unix.ICRNL
+
+	// Turn off all output processing (disable "\n" → "\r\n" translation)
+	termios.Oflag &^= unix.OPOST
 
 	// Apply updated settings immediately
 	return unix.IoctlSetTermios(fd, unix.TCSETS, termios)
